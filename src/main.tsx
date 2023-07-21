3df65dd6fbf776d2b31b90ce09e5bc301ffa76b7
Thomas Frivold <thomas.frivold@whereby.com>
2023-07-21 14:41:32 +0100

    Make it possible to pass in room url and display name

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,12 @@ import makeIntegration from "./integration.ts";
 
 makeIntegration(store, participantListAtom);
 
+const urlParams = new URLSearchParams(window.location.search);
+const roomUrl = urlParams.get("roomUrl") || "";
+const displayName = urlParams.get("displayName") || "";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <App roomUrl={roomUrl} displayName={displayName} />
   </Provider>
 );
