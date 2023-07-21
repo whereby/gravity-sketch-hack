77af508fcc7b6454feab4fe3750230a8123b1adb
Thomas Frivold <thomas.frivold@whereby.com>
2023-07-21 12:27:46 +0100

    Fix vite process.env not defined bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
+import { useRoomConnection } from "@whereby.com/browser-sdk";
 
 import "./App.css";
 
 function App() {
-  return <div>Helloooo</div>;
+  const roomName =
+    "https://funtimes.whereby.com/gs-stable-canvas8fad2b29-c7d7-4ea9-909b-4428c2d5c958";
+
+  const roomConnection = useRoomConnection(roomName, {
+    displayName: "Test",
+    localMediaConstraints: {
+      audio: true,
+      video: true,
+    },
+  });
+
+  const { state: roomState } = roomConnection;
+
+  console.log(roomState);
+  return <div>heehe</div>;
 }
 
 export default App;
